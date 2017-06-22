5a38ba417f775d1a82f608f53d3ea357e2a46345
Librazy <im@librazy.org>
2017-06-22 09:25:06 +0800

    -

--- src/test/java/cn/edu/xmu/yeahbuddy/ApplicationTests.java
+++ src/test/java/cn/edu/xmu/yeahbuddy/ApplicationTests.java
@@ -6,12 +6,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @AutoConfigureTestDatabase
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@SpringBootTest
 public class ApplicationTests {
 
     @Autowired
