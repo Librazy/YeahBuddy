30338cb830558147b441b10b38a645a1779b37ef
Librazy <im@librazy.org>
2017-07-05 15:30:43 +0800

    deployment

--- src/main/java/cn/edu/xmu/yeahbuddy/Application.java
+++ src/main/java/cn/edu/xmu/yeahbuddy/Application.java
@@ -19,6 +19,8 @@ import org.jetbrains.annotations.TestOnly;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -27,13 +29,18 @@ import java.util.Collections;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     @NonNls
     private static Log log = LogFactory.getLog(MainController.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 
     @Bean
